3ba326acf3cd317507a0ebcb3d6ce7b6a2dbccbf
AlexLenk <alexlenk.ru@gmail.com>
2022-10-18 09:00:24 +0300

    Created task two

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import './index.css'
+import img from './assets/achievements.png';
+
 console.log('Hello World!')
+
+const app = document.querySelector('#app')
+const imgEl = document.createElement('img')
+imgEl.classList.add('media')
+imgEl.src = img
+
+const h1 = document.createElement('h1')
+h1.textContent = 'I love JavaScript'
+
+app.append(imgEl, h1)
--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,9 @@
+import {babel} from '@rollup/plugin-babel'
+import styles from 'rollup-plugin-styles'
+import image from 'rollup-plugin-img'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 export default [
   {
     input: './index.js',
@@ -9,6 +15,19 @@ export default [
       exclude: 'node_modules/**'
     },
     plugins: [
+      babel({babelHelpers: 'bundled'}),
+      styles(),
+      image({
+        limit: 10000000
+      }),
+      serve({
+        open: true,
+        contentBase: './',
+        port: 3506,
+      }),
+      livereload({
+        watch: './',
+      })
     ],
   }
 ]
